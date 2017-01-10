f10044e63e9a590aa58952c3fdc4a83f21ceba27
sicacho <trustme013@gmail.com>
2017-01-10 21:55:49 +0700

    test vtt

--- src/test/java/TestRenderVTT.java
+++ src/test/java/TestRenderVTT.java
@@ -9,6 +9,6 @@ public class TestRenderVTT {
   @Test
   public void testRenderSuccessful() {
     ThumbnailExtractor thumbnailExtractor = new ThumbnailExtractor();
-    thumbnailExtractor.renderVTTFile("avb","thumnailaaa",2160,480,4383);
+//    thumbnailExtractor.renderVTTFile("avb","thumnailaaa",2160,480,4383);
   }
 }
